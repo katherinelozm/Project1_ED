84bcc3aadac7efeff4fbc4f763596881361da183
Katherine Lozano <katherinelozm@gmail.com>
2015-05-14 14:46:58 -0600

    Add of FSArrayList.h

---
+++ fsarraylist.h
@@ -0,0 +1,28 @@
+#ifndef FSARRAYLIST_H
+#define FSARRAYLIST_H
+
+#include "object.h"
+#include "tdalist.h"
+
+class FSArrayList : public TDAList{
+  protected:
+    int capacity;
+    Object** data;
+  public:
+    FSArrayList(int);
+    virtual ~FSArrayList();
+    virtual bool insert(Object*, int) ;
+    virtual int indexOf(Object*)const ;
+    virtual Object* get(unsigned)const ;
+    virtual bool erase(unsigned) ;
+    virtual int prev(int) const ;
+    virtual int next(int) const ;
+    virtual void reset() ;
+    virtual Object* first()const ;
+    virtual Object* last()const ;
+    virtual void print()const ;
+    virtual bool isFull()const ;
+    virtual int getCapacity()const;
+};
+
+#endif
