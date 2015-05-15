04a5da88b2df80331cafbd18ad3f2533e9f60df5
Katherine Lozano <katherinelozm@gmail.com>
2015-05-14 19:44:39 -0600

    Add of FSCursorList.h

---
+++ fscursorlist.h
@@ -0,0 +1,33 @@
+#ifndef FSCURSORLIST_H
+#define FSCURSORLIST_H
+
+#include "object.h"
+#include "tdalist.h"
+
+class FSCursorList : public TDAList{
+  protected:
+    struct Row{
+        int next;
+        Object* data;
+    };
+    int capacity;
+    Row* rows;
+    int head;
+  public:
+    FSCursorList(int);
+    virtual ~FSCursorList();
+    virtual bool insert(Object*, int) ;
+    virtual int indexOf(Object*)const ;
+    virtual Object* get(unsigned)const ;
+    virtual bool erase(unsigned) ;
+    virtual int prev(int) const ;
+    virtual int next(int) const ;
+    virtual void reset() ;
+    virtual Object* first()const ;
+    virtual Object* last()const ;
+    virtual void print()const ;
+    virtual bool isFull()const ;
+    virtual int getCapacity()const;
+};
+
+#endif
